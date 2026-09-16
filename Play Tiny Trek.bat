df7d2e5cb90083c39a722bf07cd36da287f20ebf
Jerry Koller <jerry.t.koller@outlook.com>
2026-09-15 22:51:43 -0500

    Sniper hand seat, locked reload, recoil, dropped mag

--- Play Tiny Trek.bat
+++ Play Tiny Trek.bat
@@ -4,5 +4,5 @@ set PORT=8766
 for /f "tokens=5" %%a in ('netstat -ano ^| findstr ":%PORT% .*LISTENING"') do taskkill /PID %%a /F >nul 2>&1
 start "Tiny Trek Server" /D "%~dp0" /min C:\Windows\py.exe -m http.server %PORT%
 ping 127.0.0.1 -n 3 >nul
-start "" "http://127.0.0.1:%PORT%/index.html?kick=1"
+start "" "http://127.0.0.1:%PORT%/index.html?sniper=1"
 exit
